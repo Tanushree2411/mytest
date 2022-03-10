b08a151132690c2522996e2dae34a6a10dec72fd
Tanushree2411 <98163393+Tanushree2411@users.noreply.github.com>
2022-03-10 07:56:02 +0530

    Add files via upload

---
+++ ptr.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class demo
+{
+int i;
+public:
+demo(int x)
+{
+i=x;
+}
+int getdata()
+{
+return i;
+}
+};
+int main()
+{
+demo d[3]={55,65,77};
+demo *ptr=d;
+for(int i=0;i<3;i++)
+{
+cout<<ptr->getdata()<<endl;
+ptr++;
+}
+}
